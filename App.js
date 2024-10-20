ff18da74ec0be06d24fe7ab2207b11fc25038a62
rsingh-16 <rsingh16@binghamton.edu>
2024-10-20 15:26:40 -0400

    React hello with different files for html, css

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
-const heading = React.createElement("h1",{id:"h1"}, "Hello From React");
+const parent = React.createElement("div", {id:"parent"}, 
+    React.createElement("div", {id:"child"}, 
+        [React.createElement("h1", {}, "I am H1"),
+            React.createElement("h2", {}, "I am H2")]));
         const root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(heading);
+        root.render(parent);
